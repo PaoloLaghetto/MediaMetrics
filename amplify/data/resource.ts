901bc48eed80fe6e3983392184587c8b9f25a20e
Paolo Laghetto <paolo.laghetto@gmail.com>
2025-01-14 18:56:02 +0100

    configure amplify

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -6,6 +6,7 @@ adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any unauthenticated user can "create", "read", "update", 
 and "delete" any "Todo" records.
 =========================================================================*/
+/*
 const schema = a.schema({
   Todo: a
     .model({
@@ -22,6 +23,27 @@ export const data = defineData({
     defaultAuthorizationMode: 'iam',
   },
 });
+*/
+
+const schema = a.schema({
+  Note: a
+    .model({
+      name:a.string(),
+      description: a.string(),
+      image: a.string(),
+    })
+    .authorization((allow) => [allow.owner()]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: 'userPool',
+  },
+});
+
 
 /*== STEP 2 ===============================================================
 Go to your frontend source code. From your client-side code, generate a
